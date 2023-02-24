50b63b1a50b9d99c3409142c22a393151ac42ffd
isaackumi <isaac.kumi@ashesi.edu.gh>
2023-02-24 16:39:03 +0000

    feat: csv file updated

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def get_contacts():
     post_data = rq.json
     query = {}
     query['api_key'] = API_KEY
-    query['q_organization_domains'] = "apollo.io\ngoogle.com"
+    # query['q_organization_domains'] = "apollo.io\ngoogle.com"
+    query['q_organization_domains'] = "apollo.io"
     query['page'] = post_data.get('page')
     query['page_titles'] = post_data.get('person_titles')
     query['per_page'] = post_data.get('per_page')
@@ -34,25 +35,64 @@ def get_contacts():
     extracted_data = []
     csv_headers = [
     
-                "first_name","last_name", "full_name","title","sanitized_phone","linkedin_url"
-                ,"photo_url","twitter_url"
+                "first_name","last_name","title", "company", "Company Names for Emails", "Email",
+                "Email Status",  "Email Confidence", "Seniority", "Department", "Departments",
+                "Contact Owner", "First Phone", "Work Direct Phone", "Home Phone", "Mobile Phone"
+                "Corporate Phone", "Other Phone", "Stage","List", "Last Contacted", "Account Owner",
+                "#Employees", "Industry","KeyWords","Person LinkedIn Url","Website", "company LinkedIn Url",
+                "Facebook Url", "Twitter Url", "City", "State", "Country", 
+                "Company Address"
                 
             ]
     for person in range(len(people)):
         extracted_data.append([
              people[person].get('first_name'),
                     people[person].get('last_name'),
-                    people[person].get('name'),
                     people[person].get('title'),
+                    people[person].get('employment_history')[0].get('organization_name'),
+                    people[person].get('employment_history')[0].get('organization_name'), # verify from api res again
+                    people[person].get('email'),
+                    people[person].get('email_status'),
+                    people[person].get('extrapolated_email_confidence'),
+                    people[person].get('seniority'),
+                    [dept for dept in people[person].get('departments')],
+                    "john.aacht@cloudfruition.com", # get auth user email
                     people[person].get('sanitized_phone'),
+                    "",
+                    "",
+                    "",
+                    people[person].get('sanitized_phone'),
+                    "",
+                    "Stage/NaN",
+                    "List/NaN",
+                    "NaN",
+                    "john.aacht@cloudfruition.com", # get auth user email,
+                    "#Employees",
+                    "#Industry",
+                    "#Keywords",
                     people[person].get('linkedin_url'),
-                    people[person].get('photo_url'),
-                    people[person].get('twitter_url'),
+                    "#Website url",
+                    "#company linkedin url",
+                    "#company Facebook url",
+                    "#company twitter url",
+                    people[person].get('city'),
+                    people[person].get('state'),
+                    people[person].get('country'),
+
+                    
+
+
+
+                    
+        #             people[person].get('sanitized_phone'),
+        #             people[person].get('linkedin_url'),
+        #             people[person].get('photo_url'),
+        #             people[person].get('twitter_url'),
         ])
 
-    
+    # filepath = os.path.join(app.root_path,'extracted_data/extracted_data.csv')
 # save CSV to local machine
-    with open('{}.csv'.format('extracted_data'),'w', encoding='UTF8', newline="") as file:
+    with open(os.path.join(app.root_path,'extracted_data/extracted_data.csv'),'w', encoding='UTF8', newline="") as file:
                 writer = csv.writer(file)
                 print("Writing data to CSV.....")
                 writer.writerow(csv_headers)
@@ -60,7 +100,8 @@ def get_contacts():
 
 # save CSV to S3 bucket
 
-    return extracted_data
+# TODO: return value should be the link to the file stored in s3 bucket
+    return people
 
 
 
